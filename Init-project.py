cb4853408e0d6ac77c2cf7a353ec463ee2c07cc6
Magda Pawłowska <magdalena.pawlowska5@gmail.com>
2021-06-23 08:56:06 +0200

    Add Init-project.py

---
+++ Init-project.py
@@ -0,0 +1,6 @@
+shopping_dictionary = {
+    'optyk' : ['okulary', 'soczewki', 'etui'],
+    'apteka' : ['paracetamol', 'bandaż', 'octenisept']
+}
+for i, j in shopping_dictionary.items():
+    print ('idę do', i, ',','kupuję', j)
