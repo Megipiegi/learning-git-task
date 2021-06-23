c90fc22c6e6dfb4166e44d9048a0d6b7386fb7de
Magda Pawłowska <magdalena.pawlowska5@gmail.com>
2021-06-23 14:08:46 +0200

    Add Test-Magda.py

--- Init-project.py
+++ Init-project.py
@@ -1,6 +1,10 @@
 shopping_dictionary = {
     'optyk' : ['okulary', 'soczewki', 'etui'],
-    'apteka' : ['paracetamol', 'bandaż', 'octenisept']
+    'apteka' : ['paracetamol', 'bandaż']
 }
 for i, j in shopping_dictionary.items():
-    print ('idę do', i, ',','kupuję', j)
+    print('Idę do', i.title(),',', 'kupuję tu następujące rzeczy', str(j).title())
+total_elements=0
+for j in shopping_dictionary:
+    total_elements += len (shopping_dictionary[j])
+print('w sumie kupuję:', total_elements, 'produktów')
---
+++ Test-Magda.py
@@ -0,0 +1,1 @@
+print ('Hello World! Magda Magda Test')
