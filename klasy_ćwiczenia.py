e265be66ffcbfd6435a9d99174e05b3061000470
Magda Pawłowska <magdalena.pawlowska5@gmail.com>
2021-07-08 09:16:10 +0200

    dodane klasy i obiekty

--- klasy_ćwiczenia.py
+++ klasy_ćwiczenia.py
@@ -1,5 +1,10 @@
 class Car:
-   pass
-my_car=Car()
-type(my_car)
+    def __init__ (self, make, model_name, top_speed, colour):
+        self.make=make
+        self.model=model_name
+        self.top_speed=top_speed
+        self.colour=colour
+mustang=Car(make='Ford', model_name='Mustang', top_speed='250', colour='yellow')
+print(mustang.make)
+
 
