aaef9e88cee728fee452ca739177f6ad2257bd58
Magda Pawłowska <magdalena.pawlowska5@gmail.com>
2021-07-08 10:17:11 +0200

    spr

--- Funkcje_ćwiczenia.py
+++ Funkcje_ćwiczenia.py
@@ -1,1 +1,8 @@
-print('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie')
+class Car:
+    def __init__ (self, make, model_name, top_speed, colour):
+        self.make=make
+        self.model=model_name
+        self.top_speed=top_speed
+        self.colour=colour
+mustang=Car(make='Ford', model_name='Mustang', top_speed='250', colour='yellow')
+print(mustang.make)
--- Pozdrowienia.py
+++ Pozdrowienia.py
@@ -1,6 +1,8 @@
-print ("enter input number : ")
-input1 = int(raw_input())
-sum1 = 0
-for y in range(0,input1+1):
-       sum1 = sum1 + y
-print ("the sum is " + str(sum1))
+class Car:
+    def __init__ (self, make, model_name, top_speed, colour):
+        self.make=make
+        self.model=model_name
+        self.top_speed=top_speed
+        self.colour=colour
+mustang=Car(make='Ford', model_name='Mustang', top_speed='250', colour='yellow')
+print(mustang.make)
