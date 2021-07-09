93056aa1b68320aa312c1d9585d647b08b14fe62
Magda Pawłowska <magdalena.pawlowska5@gmail.com>
2021-07-09 09:01:28 +0200

    zadanie z Car

--- Test-Magda.py
+++ Test-Magda.py
@@ -1,20 +1,13 @@
-question=input('podaj swoje imię: ')
-print('Witaj', question)
-
-print ('Witaj, ten program pomoże Ci sprawdzić, czy jesteś pełnolteni/a')
-adult = None
-sex = input ('Podaj proszę swoją płeć [M/K]: ')
-if sex == 'M':
-    age=int(input('Twój wiek?'))
-    adult = age>=18
-elif sex == 'K':
-    print ('Kobiet się o wiek nie pyta, więc zrobię to delikatnie:')
-    over18yes_no= input('Czy miałaś już 18-tkę? [T/N]?')
-    adult=(over18yes_no == 'T')
-else:
-    print('nie ma takiej płci!')
-    exit (1)
-print ('już wiem, Twoja pełnoletność w boolean to %s ' %str (adult))
-# Przy okazji, zwróć uwagę na wykorzystanie funkcji exit(), która kończy działanie programu. Jeśli podamy błędną płeć, wywołamy exit z parametrem 1, który oznacza, że nastąpił pewien błąd. Zwykle, gdy program kończy się poprawnie, to rezultat skryptu ma wartość 0.
+class Car:
+    def __init__(self, make, model_name, top_speed, color):
+       self.make = make
+       self.model_name = model_name
+       self.top_speed = top_speed
+       self.color = color
+    def __str__(self):
+        return f'{self.color} {self.make} {self.model_name}'
+    
+mustang = Car(make="Ford", model_name="Mustang", color="Yellow", top_speed=250)
+print(mustang)
 
 
