c27793511b3f74e7fc0cca1b217aaa5a862b04dd
Magda Pawłowska <magdalena.pawlowska5@gmail.com>
2021-07-09 10:35:49 +0200

    jeszcz raz odkomentowany print przy add

--- Pozdrowienia.py
+++ Pozdrowienia.py
@@ -4,5 +4,11 @@ class Car:
         self.model=model_name
         self.top_speed=top_speed
         self.colour=colour
-mustang=Car(make='Ford', model_name='Mustang', top_speed='250', colour='yellow')
-print(mustang.make)
+        self.current_speed = 0
+car = Car(make="Ford", model_name="Mustang", top_speed=250, colour="Red")
+    def accelerate(self, step=10):
+        self.current_speed += step
+    def decelerate(self, step=10):
+        self.current_speed -= step
+car.top_speed
+print(car)
